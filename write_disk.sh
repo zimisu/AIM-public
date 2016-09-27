e3caf4d7e6322d7e8923eddcb89e36aa41ef3a1a
Chen Kan <zimisu@gmail.com>
2016-09-27 17:59:32 +0800

    re-create disk and modify write_disk.sh

--- write_disk.sh
+++ write_disk.sh
@@ -1,6 +1,7 @@
 #./configure --host=i386-pc-elf MACH=pc ARCH=i386
 make
 dd if=boot/boot.bin of=$HOME/big_disk.img bs=446 count=1 conv=notrunc
-dd if=kern/vmaim.elf of=$HOME/big_disk.img bs=512 count=1 conv=notrunc seek=204801
+#sudo dd if=kern/vmaim.elf of=/dev/loop2p2 bs=512 count=100 conv=notrunc 
+dd if=kern/vmaim.elf of=$HOME/big_disk.img bs=512 count=100 conv=notrunc seek=204800
 
 #qemu-system-i386 big_disk.img -gdb tcp::1234 -S
