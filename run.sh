e74ca70dbe4f77a37d868996fece27d4f94e64a9
Chen Kan <zimisu@gmail.com>
2016-10-25 21:52:58 +0800

    add "-serial stdio" in qemu comman

--- run.sh
+++ run.sh
@@ -1,2 +1,2 @@
-gnome-terminal -x zsh -c 'cd ~/AIM-public; sh write_disk.sh; qemu-system-i386 $HOME/big_disk.img -gdb tcp::1234 -S;' &''
+gnome-terminal -x zsh -c 'cd ~/AIM-public; sh write_disk.sh; qemu-system-i386 $HOME/big_disk.img -serial stdio -gdb tcp::1234 -S;' &''
 gnome-terminal -x bash -c "cd ~/AIM-public; $CROSS_GDB kern/vmaim.elf"
